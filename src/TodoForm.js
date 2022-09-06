10c85b9a94c9ac6a3d1fc6a748d4822c31004594
Mahdi Heydari <mahdiheydari15879@gmail.com>
2022-09-06 13:32:59 +0430

    change background color of splashscreen

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import useInputState from "./hooks/useInputState";
+
 function TodoForm({ addTodo }) {
   const [value, handleChange, reset] = useInputState("");
   return (
